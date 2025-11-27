e812b361ed78acd8432ad983009c56ef6bc8b6c1
The-Hell-Blazer <109266975+The-Hell-Blazer@users.noreply.github.com>
2025-11-27 14:12:53 +0530

    Update a2l_updater.py

--- a2l_updater.py
+++ a2l_updater.py
@@ -64,7 +64,7 @@ def update_a2l_file(a2l_file, address_map):
 
     <h3>Address Change Analysis Report</h3>
 
-    <canvas id="barChart" width="1000" height="750"></canvas>
+    <canvas id="barChart" width="750" height="750"></canvas>
 
     <script>
     var ctx = document.getElementById('barChart').getContext('2d');
